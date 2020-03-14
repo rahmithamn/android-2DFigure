ffa4bf6254a42b14d9940896b03a102f9beaacc8
rahmithamn <rahmithanisa@gmail.com>
2020-03-14 20:48:11 +0700

    Add MenuItem and NavigateUp

--- app/src/main/java/org/rmn/a2dfigure/MainFragment.kt
+++ app/src/main/java/org/rmn/a2dfigure/MainFragment.kt
@@ -2,12 +2,11 @@ package org.rmn.a2dfigure
 
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import org.rmn.a2dfigure.databinding.FragmentMainBinding
 
 /**
@@ -28,8 +27,18 @@ class MainFragment : Fragment() {
         binding.btnSegitiga.setOnClickListener { v: View ->
             v.findNavController().navigate(R.id.action_mainFragment_to_segitigaFragment)
         }
+        setHasOptionsMenu(true)
         return binding.root
     }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.main, menu)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!,
+            view!!.findNavController())
+                || super.onOptionsItemSelected(item)
+    }
 
 }
