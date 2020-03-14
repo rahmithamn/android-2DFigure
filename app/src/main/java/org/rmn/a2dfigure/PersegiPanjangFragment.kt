6ee1eddec870d5e90d9469c109947d663a91902a
rahmithamn <rahmithanisa@gmail.com>
2020-03-14 22:10:00 +0700

    Add Formula to count

--- app/src/main/java/org/rmn/a2dfigure/PersegiPanjangFragment.kt
+++ app/src/main/java/org/rmn/a2dfigure/PersegiPanjangFragment.kt
@@ -1,23 +1,48 @@
 package org.rmn.a2dfigure
 
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import kotlinx.android.synthetic.main.fragment_persegi_panjang.*
+import org.rmn.a2dfigure.databinding.FragmentPersegiPanjangBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class PersegiPanjangFragment : Fragment() {
+    private var luasPersegiPanjang = 0;
+    private var kelilingPersegiPanjang = 0;
 
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_persegi_panjang, container, false)
+        val binding: FragmentPersegiPanjangBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_persegi_panjang, container, false)
+        binding.btnHitungpersegipanjang.setOnClickListener {v: View->
+            if (!TextUtils.isEmpty(et_panjang.text.toString()) && !TextUtils.isEmpty(et_lebar.text.toString())) {
+                var panjang = et_panjang.text.toString().toInt()
+                var lebar = et_lebar.text.toString().toInt()
+
+                luasPersegiPanjang = panjang * lebar
+                kelilingPersegiPanjang = (2 * panjang) + (2 * lebar)
+
+                binding.tvLuaspersegipanjang.text = "Luas = $luasPersegiPanjang"
+                binding.tvKelilingpersegipanjang.text = "Keliling = $kelilingPersegiPanjang"
+            } else {
+                Toast.makeText(this.activity, "Isian Ada yang kosong", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        return binding.root
     }
 
 
--- app/src/main/java/org/rmn/a2dfigure/SegitigaFragment.kt
+++ app/src/main/java/org/rmn/a2dfigure/SegitigaFragment.kt
@@ -1,23 +1,54 @@
 package org.rmn.a2dfigure
 
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import kotlinx.android.synthetic.main.fragment_segitiga.*
+import org.rmn.a2dfigure.databinding.FragmentSegitigaBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class SegitigaFragment : Fragment() {
+    private var luasSegitiga = 0.0
+    private var kelilingSegitiga = 0.0
 
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_segitiga, container, false)
+        val binding: FragmentSegitigaBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_segitiga, container, false)
+        binding.btnHitungsegitiga.setOnClickListener { v:View->
+            if (!TextUtils.isEmpty(et_alas.text.toString()) && !TextUtils.isEmpty(et_tinggi.text.toString())) {
+                var alas = et_alas.text.toString().toDouble()
+                var tinggi = et_tinggi.text.toString().toDouble()
+
+                var alasSquare = alas * alas
+                var tinggiSquare = tinggi * tinggi
+
+                var cariSisiMiring = alasSquare + tinggiSquare
+
+
+                luasSegitiga = (alas * tinggi) / 2
+                kelilingSegitiga = Math.sqrt(cariSisiMiring) + alas + tinggi
+
+                binding.tvLuassegitiga.text= "Luas = $luasSegitiga"
+                binding.tvKelilingsegitiga.text = "Keliling = $kelilingSegitiga"
+            }else {
+                Toast.makeText(this.activity, "Isian ada yang kosong", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        return binding.root
     }
 
 
